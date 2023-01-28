acfb7b35e6cc96156fd0271c8c7210b39c76377b
kreketjot <kreketjot@gmail.com>
2023-01-28 16:04:56 +0000

    Update 2 files

--- src/shared/api/typicode/tasks.ts
+++ src/shared/api/typicode/tasks.ts
@@ -1,22 +1,33 @@
-import { AxiosPromise } from 'axios';
-import { apiInstance } from './base';
-import type { Task } from './models';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = '/todos';
+import type { Task } from "./models";
+import { API_URL } from "../../config";
+const BASE_URL = "/todos";
 
 export type GetTasksListParams = {
-  userId?: number
-  completed?: boolean
-}
+  userId?: number;
+  completed?: boolean;
+};
 
 export const getTasksList = (
-  params?: GetTasksListParams,
+  params?: GetTasksListParams
 ): AxiosPromise<Task[]> => apiInstance.get(BASE_URL, { params });
 
 export type GetTaskByIdParams = {
-  taskId: number
-}
+  taskId: number;
+};
 
-export const getTaskById = (
-  params: GetTaskByIdParams,
-): AxiosPromise<Task> => apiInstance.get(`${BASE_URL}/${params.taskId}`, { params });
+export const getTaskById = (params: GetTaskByIdParams): AxiosPromise<Task> =>
+  apiInstance.get(`${BASE_URL}/${params.taskId}`, { params });
+
+export const typicodeApi = createApi({
+  reducerPath: 'typicodeApi',
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL })
+  endpoints: (builder) => ({
+    getTasksList: builder.query<Task[], GetTasksListParams>({
+      query: (params) => BASE_URL,
+    })
+  })
+})
+
+export const { useGetTasksList } = typicodeApi;
